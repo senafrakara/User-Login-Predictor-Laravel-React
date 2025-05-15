3156b806c3417a99d5635edb3222e0d2f9359425
Nursena  Karakulah <senafrakara@gmail.com>
2025-05-15 12:34:31 +0300

    main blade update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/dist/styles.css" rel="stylesheet">
-    <title>Laravel + Vite + React</title>
+    {{--     <link href="/dist/styles.css" rel="stylesheet">
+ --}} <title>Laravel + Vite + React</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
